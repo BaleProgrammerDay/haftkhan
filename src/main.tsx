d91b492708850fea455d065a3dac36a6322573c5
h.soleymani <hesamsoleymani20@gmail.com>
2025-09-11 14:37:55 +0330

    fixes messanger

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { NotificationProvider } from "./context/Notification.tsx";
+import Messanger from "./pages/khan-7/messenger/Messanger.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <NotificationProvider>
     <App />
+    {/* <Messanger /> */}
   </NotificationProvider>
   // </React.StrictMode>
 );
-
